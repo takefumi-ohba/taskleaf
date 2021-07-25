90afc2fa6f771765a695327d5d7b58aa661fd7cc
takefumi-ohba <t_ohba24@yahoo.co.jp>
2021-07-25 13:52:13 +0900

    管理者権限をもつユーザーのページを作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,12 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -12,15 +20,24 @@ class Admin::UsersController < ApplicationController
     end
   end
 
-
-
   def edit
+    @user = User.find(params[:id])
   end
 
-  def show
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
+    else
+      render :new
+    end
   end
 
-  def index
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}」を削除しました。"
   end
 
   private
