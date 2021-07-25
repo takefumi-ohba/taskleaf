34470c9b6babafb8cc25011e9085bc702b31a783
takefumi-ohba <t_ohba24@yahoo.co.jp>
2021-07-25 13:32:46 +0900

    usersテーブルにadminカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_25_042201) do
+ActiveRecord::Schema.define(version: 2021_07_25_043041) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", limit: 30, null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_07_25_042201) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
